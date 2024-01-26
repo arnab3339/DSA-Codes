b91cd7a82d15f85232930ac68c7fb51330a72a66
Arnab Mukherjee <139433951+arnab3339@users.noreply.github.com>
2024-01-26 21:46:26 +0530

    Update Stack operations without using class.cpp

--- Stack operations without using class.cpp
+++ Stack operations without using class.cpp
@@ -1,62 +1,68 @@
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
+
 #define max 5
 int stack[max], top = -1;
-void push(int val){
-	if(top >= max-1){
-		cout<<"Overflow";
-		}
-	else
-		stack[++top] = val;
-	}
-void pop(){
-	if(top <= -1)
-		cout<<"Underflow";
-	else{
-		cout<<"Popped element"<<stack[top];
-		top--;
-	}
+
+void push(int value) {
+    if (top >= max - 1) {
+        cout << "Stack Overflow";
+    } else {
+        top = top + 1;
+        stack[top] = value;
+    }
 }
-void display(){
-	int i;
-		if(top >=0){
-			cout<<"Stack elements are : ";
-			for(i=top; i >=0; i--)
-			cout<<stack[i]<<" ";
-			}
-		else
-			cout<<"Stack is empty";
-			}
-int main(){
-	int ch, val;
-	do{
-		cout<<endl;
-		cout<<"1) Push"<<endl;
-		cout<<"2) Pop"<<endl;
-		cout<<"3) Display"<<endl;
-		cout<<"4) Exit"<<endl;
-		cout<<"Enter your choice : ";
-		cin>>ch;
-		switch(ch){
-		case 1:
-			cout<<"Enter the value to be inserted : ";
-			cin>>val;
-			push(val);
-			break;
-		case 2: 
-			pop();
-			break;
-		case 3: 
-			display();
-			break;
-		case 4:
-			exit(0);
-			break;
-		default:
-			cout<<"Wrong Choice";
-		}
-	}	
-while(ch != 4);
-return 0;
+
+void pop() {
+    if (top <= -1) {
+        cout << "Stack Underflow";
+    } else {
+        cout << "Popped element: " << stack[top];
+        top = top - 1;
+    }
+}
+
+void display() {
+    int i;
+    if (top >= 0) {
+        cout << "Stack elements are: ";
+        for (i = top; i >= 0; i--) {
+            cout << stack[i] << " ";
+        }
+    } else {
+        cout << "Stack is Empty";
+    }
+}
+
+int main() {
+    int ch, value;
+    do {
+        cout << endl;
+        cout << "1. Push" << endl;
+        cout << "2. Pop" << endl;
+        cout << "3. Display" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter Your Choice: ";
+        cin >> ch;
+        switch (ch) {
+            case 1:
+                cout << "Enter the value to push: ";
+                cin >> value;
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+                break;
+            default:
+                cout << "Wrong Choice";
+        }
+    } while (ch != 4);
+    return 0;
 }
